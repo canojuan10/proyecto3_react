c2ebade668f7b0fef7209cb37c9d156845387c56
hack <hugolgsuarez@gmail.com>
2022-05-31 10:26:05 +0200

    header_footer

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import { UserDetail } from "./view/UserDetail/UserDetail";
 import { NewDetail } from "./view/NewDetail/NewDetail";
 import { NotFound } from "./view/NotFound/NotFound";
 import { Register } from "./view/Register/Register";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
@@ -18,6 +21,7 @@ function App() {
         <Route path="/register" element={<Register />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
